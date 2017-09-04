c901bf11a8219cec4a7c5cccb707c6254070f3ef
LisavetaTsyulia <tsyulialisa@gmail.com>
2017-09-04 14:19:51 +0300

    start routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
 import { AppComponent } from './app.component';
-
-import { LoginComponent } from './components/login/login.component';
 import { HeaderComponent } from './components/header/header.component';
+import {AppRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
     HeaderComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
