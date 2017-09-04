b7566596023e6628b1bea6ae1adf672d7e24bb04
LisavetaTsyulia <tsyulialisa@gmail.com>
2017-09-04 11:07:14 +0300

    Simple login form is made

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { User }    from '../user/user';
 
 @Component({
-  selector: 'app-login',
+  selector: 'login-form',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  model = new User('', '');
+  submitted = false;
+  onSubmit() { this.submitted = true; }
+
+  get diagnostic() { return JSON.stringify(this.model); }
+
+  newHero() {
+    this.model = new User('', '');
+  }
 
   constructor() { }
 
---
+++ src/app/components/user/user.ts
@@ -0,0 +1,8 @@
+export class User {
+
+  constructor(
+    public password: string,
+    public email: string
+  ) {  }
+
+}
