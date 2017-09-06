dd872fbbae2e6b5f58a43d55cd0329e41a251311
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-06 20:44:43 +0300

    Made login form verification

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { User } from '../user/user';
 import {AuthService} from '../auth/auth.service';
 import {ActivatedRoute, Router} from '@angular/router';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {EmailValidators} from "../validators/EmailValidators";
+import {PasswordValidators} from "../validators/PasswordValidators";
 
 @Component({
   selector: 'app-login-form',
@@ -12,6 +15,7 @@ export class LoginComponent implements OnInit {
   model = new User;
   submitted = false;
   returnUrl: string;
+  formGroup: FormGroup;
 
   get diagnostic() { return JSON.stringify(this.model); }
 
@@ -22,11 +26,17 @@ export class LoginComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
     this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+
+    this.formGroup = this.fb.group({
+      email: ['', [Validators.required, Validators.minLength(6), EmailValidators.isValidEmail]],
+      password: ['', Validators.required],
+    });
   }
 
   onSubmit() {
--- src/app/components/login/login.module.ts
+++ src/app/components/login/login.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import {RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {LoginPageGuard} from './login-page.guard';
 import {AuthModule} from '../auth/auth.module';
 
@@ -16,7 +16,8 @@ import {AuthModule} from '../auth/auth.module';
     ]),
     CommonModule,
     FormsModule,
-    AuthModule
+    AuthModule,
+    ReactiveFormsModule
   ],
   providers: [
     LoginPageGuard,
