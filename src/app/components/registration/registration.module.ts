46d9b2fd1f73713877170b75e65882afafc30408
LisavetaTsyulia <tsyulialisa@gmail.com>
2017-09-07 11:58:10 +0300

    Registration works but there are no errors displayed

--- src/app/components/registration/registration.module.ts
+++ src/app/components/registration/registration.module.ts
@@ -3,15 +3,23 @@ import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration.component';
 import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {LoginPageGuard} from '../login/login-page.guard';
+import {AuthModule} from '../auth/auth.module';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
-      { path: '', component: RegistrationComponent }
+      { path: '',
+        component: RegistrationComponent,
+        canActivate: [LoginPageGuard] }
     ]),
     CommonModule,
     FormsModule,
+    AuthModule,
     ReactiveFormsModule
+  ],
+  providers: [
+    LoginPageGuard
   ],
   declarations: [
     RegistrationComponent
