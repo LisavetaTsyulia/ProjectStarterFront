0f8c4075547d8506618ecb17902e4bf22c366ef6
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-12 17:46:08 +0300

    Project info component created and added routing for it

---
+++ src/app/components/project/project-info/project-info.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {AuthModule} from '../../auth/auth.module';
+import {ProjectService} from '../project.service';
+import { DatePickerModule } from 'ng2-datepicker';
+import {ProjectInfoComponent} from './project-info.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      { path: '',
+        component: ProjectInfoComponent}
+    ]),
+    CommonModule,
+    FormsModule,
+    AuthModule,
+    DatePickerModule
+  ],
+  declarations: [
+    ProjectInfoComponent,
+
+  ],
+  providers: [
+    ProjectService
+  ],
+  exports: [RouterModule]
+})
+export class ProjectInfoModule { }
