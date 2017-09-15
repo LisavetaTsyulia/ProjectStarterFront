e9db8084954f8a432c23deae218cbf009fbd54ce
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-15 20:43:05 +0300

    Project card component was created

--- src/app/components/project-card/project-card.module.ts
+++ src/app/components/project-card/project-card.module.ts
@@ -2,35 +2,25 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms';
-import { DatePickerModule } from 'ng2-datepicker';
-import { FileUploadModule } from 'ng2-file-upload';
-import {Ng2CloudinaryModule} from 'ng2-cloudinary';
 import {MdCardModule, MdButtonModule} from '@angular/material';
-import {HomePageComponent} from './home-page.component';
 import {AuthModule} from '../auth/auth.module';
-import {ProjectService} from '../project/project.service';
+import {ProjectCardComponent} from './project-card.component';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       { path: '',
-        component: HomePageComponent}
+        component: ProjectCardComponent}
     ]),
     CommonModule,
     FormsModule,
     AuthModule,
-    DatePickerModule,
-    Ng2CloudinaryModule,
-    FileUploadModule,
     MdCardModule,
-    MdButtonModule,
+    MdButtonModule
   ],
   declarations: [
-    HomePageComponent,
-  ],
-  providers: [
-    ProjectService
+    ProjectCardComponent,
   ],
   exports: [RouterModule]
 })
-export class HomePageModule { }
+export class ProjectCardModule { }
