dbd18887326be719c7dd192a905bb0ba2885d6f2
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-16 12:58:18 +0300

    Deleted extra components in project editing

--- src/app/components/project/project-editing/project-editing.module.ts
+++ src/app/components/project/project-editing/project-editing.module.ts
@@ -4,19 +4,15 @@ import {RouterModule} from '@angular/router';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {AuthModule} from '../../auth/auth.module';
 import {ProjectEditingComponent} from './project-editing.component';
-import { BasicsComponent } from './components/basics/basics.component';
-import { RewardsComponent } from './components/rewards/rewards.component';
-import { StoryComponent } from './components/story/story.component';
 import {EditorComponent} from '../../helpers/editor/editor.component';
 import {ProjectService} from '../project.service';
-import { GoalsComponent } from './components/goals/goals.component';
 import { DatePickerModule } from 'ng2-datepicker';
 import {Ng2CloudinaryModule} from 'ng2-cloudinary';
 import { FileUploadModule } from 'ng2-file-upload';
 import {MdDatepickerModule, MdFormFieldModule, MdNativeDateModule} from '@angular/material';
 import { Ng2FileDropModule } from 'ng2-file-drop';
 import {NewsComponent} from './components/news/news.component';
-import {ProjectCardModule} from "../../project-card/project-card.module";
+import {ProjectCardModule} from '../../project-card/project-card.module';
 
 
 @NgModule({
@@ -46,11 +42,7 @@ import {ProjectCardModule} from "../../project-card/project-card.module";
   ],
   declarations: [
     ProjectEditingComponent,
-    BasicsComponent,
-    RewardsComponent,
-    StoryComponent,
     EditorComponent,
-    GoalsComponent,
     NewsComponent,
   ],
   providers: [
