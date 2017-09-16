9905722c570ad4753981952b0707eb07a9552c05
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-16 11:51:46 +0300

    Added updating project image and title

--- src/app/components/project/project-editing/project-editing.component.ts
+++ src/app/components/project/project-editing/project-editing.component.ts
@@ -4,7 +4,7 @@ import {Subscription} from 'rxjs/Subscription';
 import {ProjectService} from '../project.service';
 import {Project} from '../../model/project';
 import { Ng2FileDropAcceptedFile } from 'ng2-file-drop';
-import {CloudinaryUploader} from 'ng2-cloudinary';
+import {CloudinaryOptions, CloudinaryUploader} from 'ng2-cloudinary';
 
 @Component({
   selector: 'app-project-editing',
@@ -12,18 +12,31 @@ import {CloudinaryUploader} from 'ng2-cloudinary';
   styleUrls: ['./project-editing.component.css']
 })
 export class ProjectEditingComponent implements OnInit, OnDestroy {
+  // Drag and drop
+  supportedFileTypes: string[] = ['image/png', 'image/jpeg', 'image/gif'];
+  imageShown = false;
+  currentProfileImage = 'assets/profile-placeholder.png';
+  uploader: CloudinaryUploader = new CloudinaryUploader(
+    new CloudinaryOptions({ cloudName: 'project-starter', uploadPreset: 'clbhkmd8' })
+  );
+
   project = new Project;
   projectId: number;
   private subscription: Subscription;
   errorMessage: string;
   successMessage: string;
 
-  uploader: CloudinaryUploader;
-
   constructor(
     private activatedRoute: ActivatedRoute,
     private projectService: ProjectService,
-  ) { }
+  ) {
+    this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any): any => {
+      const res: any = JSON.parse(response);
+      this.project.imageUrl = 'https://res.cloudinary.com/project-starter/image/upload/v1505240342/' +
+        res.public_id;
+      return { item, response, status, headers };
+    };
+  }
 
   ngOnInit() {
     this.subscription = this.activatedRoute.params.subscribe(params =>
@@ -51,14 +64,23 @@ export class ProjectEditingComponent implements OnInit, OnDestroy {
       );
   }
 
-  onUpload(uploader) {
-    this.uploader = uploader;
-    if (this.uploader) {
-      this.uploader.uploadAll();
-    }
-  }
-
   ngOnDestroy() {
     this.subscription.unsubscribe();
+  }
+
+  // File being dragged has been dropped and is valid
+  private dragFileAccepted(acceptedFile: Ng2FileDropAcceptedFile) {
+    const fileReader = new FileReader();
+    fileReader.onload = () => {
+
+      // Set and show the image
+      this.currentProfileImage = fileReader.result;
+      this.imageShown = true;
+    };
+
+    // Read in the file
+    fileReader.readAsDataURL(acceptedFile.file);
+
+    this.uploader.uploadAll();
   }
 }
