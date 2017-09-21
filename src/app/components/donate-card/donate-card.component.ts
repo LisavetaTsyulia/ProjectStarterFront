8643add779c319ef5791032a51c71209972d7737
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-21 14:25:13 +0300

    Donate cards with number

--- src/app/components/donate-card/donate-card.component.ts
+++ src/app/components/donate-card/donate-card.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import {Donate} from '../model/donate';
 import {DonateService} from './donate.service';
 import {Biography} from '../model/biography';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-donate-card',
@@ -10,10 +11,12 @@ import {Biography} from '../model/biography';
 })
 export class DonateCardComponent implements OnInit {
   @Input() donate: Donate;
+  @Input() number: number;
   biography: Biography;
 
   constructor(
-    private donateService: DonateService
+    private donateService: DonateService,
+    private router: Router,
   ) { }
 
   ngOnInit() {
@@ -26,5 +29,9 @@ export class DonateCardComponent implements OnInit {
         this.biography = new Biography;
         Object.assign(this.biography, data);
     });
+  }
+
+  viewProject() {
+    this.router.navigate(['project-info', this.donate.projectId]);
   }
 }
