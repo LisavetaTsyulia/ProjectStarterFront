f58b85a437eed0e621c7d917cac12b2d4e439a7a
Andrey Mikhadyuk <mihadiuk.andrej@gmail.com>
2017-09-23 17:48:46 +0300

    Components for projects news were added and routing to it

---
+++ src/app/components/account-menu/news-messages/news-messages.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-news-messages',
+  templateUrl: './news-messages.component.html',
+  styleUrls: ['./news-messages.component.css']
+})
+export class NewsMessagesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/components/account-menu/news-messages/news-messages.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule} from '@angular/router';
+import {FormsModule} from '@angular/forms';
+import {AuthModule} from '../../auth/auth.module';
+import {ProjectService} from '../../project/project.service';
+import {ProjectCardModule} from '../../project-card/project-card.module';
+import {TranslateModule} from '@ngx-translate/core';
+import {NewsMessagesComponent} from './news-messages.component';
+
+@NgModule({
+  imports: [
+    RouterModule.forChild([
+      { path: '',
+        component: NewsMessagesComponent}
+    ]),
+    CommonModule,
+    FormsModule,
+    AuthModule,
+    ProjectCardModule,
+    TranslateModule.forChild()
+  ],
+  declarations: [
+    NewsMessagesComponent,
+  ],
+  providers: [
+    ProjectService
+  ],
+  exports: [RouterModule]
+})
+export class NewsMessagesModule { }
